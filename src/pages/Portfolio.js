b8efb12522f5296a13183f3a3d7745d21000c4a3
Heng <bunsanphe@gmail.com>
2020-08-31 21:31:17 -0700

    updated portfolio with links and images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Container, Row, Col } from "../components/Grid";
+import { Container, Row, Col } from "react-bootstrap"
 import Image from 'react-bootstrap/Image'
 import Gaea from "../images/Gaea_screenshot.png"
+import pwGenerator from "../images/password_gen_screenshot.png"
+import planner from "../images/planner_screenshot.png"
+import quiz from "../images/quiz_screenshot.png"
+
 function Portfolio(){
     return(
-        <Container>
+        <Container fluid>
             <Row>
-                <Col size="lg-4">
+                <Col lg={9}>
                     <div className="content-box">
                         <h1>Portfolio</h1>
                         <div className="images">
-                            <Col size="lg-6">
+                            <Row>
+                            <Col lg={6}>
                                 <div className="porttext">
                                     <a href="https://saharanath.github.io/Project1/">
                                         Gaea - Hiking Trail
@@ -18,6 +23,51 @@ function Portfolio(){
                                     <img src={Gaea} class="img-fluid" alt=" "></img>
                                 </div>
                             </Col>
+                            <Col lg={6}>
+                                <div className="porttext">
+                                    <a href="https://bunsanphe.github.io/password_generator/">
+                                        Password Generator
+                                    </a>
+                                    <img src={pwGenerator} class="img-fluid" alt=" "></img>
+                                </div>
+                            </Col>
+                            </Row>
+                            <Row>
+                            <Col lg={6}>
+                                <div className="porttext">
+                                    <a href="https://bunsanphe.github.io/timed_quiz/">
+                                        Psychology Quiz
+                                    </a>
+                                    <img src={quiz} class="img-fluid" alt=" "></img>
+                                </div>
+                            </Col>
+                            <Col lg={6}>
+                                <div className="porttext">
+                                    <a href="https://bunsanphe.github.io/work_day_scheduler/">
+                                        Daily Planner
+                                    </a>
+                                    <img src={planner} class="img-fluid" alt=" "></img>
+                                </div>
+                            </Col>
+                            </Row>
+                            <Row>
+                            <Col lg={6}>
+                                <div className="porttext">
+                                    <a href="https://project-2-5588.herokuapp.com/">
+                                        ClickPlay
+                                    </a>
+                                    <img src="https://images.pexels.com/photos/2659475/pexels-photo-2659475.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" class="img-fluid" alt=" "></img>
+                                </div>
+                            </Col>
+                            <Col lg={6}>
+                                <div className="porttext">
+                                    <a href="#">
+                                        TBA
+                                    </a>
+                                    <img src="https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" class="img-fluid" alt=" "></img>
+                                </div>
+                            </Col>
+                            </Row>
                         </div>
                     </div>
                 </Col>
